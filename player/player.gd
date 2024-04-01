d36b4b64b41bdc4a8577fa18514f2820a58e2fa4
Matthew <f1driver05@gmail.com>
2024-04-01 15:22:04 +0100

    Moved camera to face the negative z, forward direction so now the input direction vector does not have to be inverted.

--- player/player.gd
+++ player/player.gd
@@ -8,7 +8,10 @@ const snowball_scene: Resource = preload("res://snowball/snowball.tscn")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var facing := Vector3.FORWARD
+var facing = Vector3.FORWARD
+
+@onready
+var direction_ball = $DirectionBall
 
 func _process(delta):
 	if Input.is_action_just_pressed("player_spawn_snowball"):
@@ -29,16 +32,16 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		facing = direction
-		velocity.x = -input_dir.x * SPEED
-		velocity.z = -input_dir.y * SPEED
+		velocity.x = input_dir.x * SPEED
+		velocity.z = input_dir.y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	move_and_slide()
-	look_at(global_transform.origin + facing, Vector3.UP)
+	direction_ball.position = 1.5 * facing
 
 func spawn_snowball():
 	var snowball = snowball_scene.instantiate()
-	snowball.position = position + 1.3 * facing
 	get_parent().add_child(snowball)
+	snowball.position = global_position + 1.5 * facing
