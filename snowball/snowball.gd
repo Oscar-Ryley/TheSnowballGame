aa6fa9824c946b1ade94115a598a8bf977ec4d9e
Matthew <f1driver05@gmail.com>
2024-04-19 22:05:57 +0100

    Made it so the players now interact with the snowballs and the snowballs now grow as they move.

--- snowball/snowball.gd
+++ snowball/snowball.gd
@@ -1,9 +1,23 @@
 extends RigidBody3D
 
+@export var growth_constant: float = 0.0005
+
+@onready var model_pivot: Node3D = $"Model Pivot"
+@onready var collision_shape: CollisionShape3D = $"CollisionShape3D"
+
 func _ready():
-	pass 
+	model_pivot.scale = 2 * Vector3.ONE
+	collision_shape.scale = 2 * Vector3.ONE
+	
 
+func _physics_process(_delta):
+	var speed: float = linear_velocity.length()
+	if speed > 0:
+		var change: float = growth_constant * speed
+		increment_children_scale(change)
+		print(change)
 
-func physics_process(_delta):
-	scale.x += linear_velocity.length()
-	print(linear_velocity.length())
+func increment_children_scale(change):
+	var new_scale_vec: Vector3 = change * Vector3.ONE
+	model_pivot.scale += new_scale_vec
+	collision_shape.scale += new_scale_vec
