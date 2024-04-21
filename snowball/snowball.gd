fd6eccaccef05fcd97d03cd9c61263e58c008ac8
Oscar Ryley <oscar.ryley@gmail.com>
2024-04-21 12:06:41 +0100

    Player 2 Snowball Growth and World Border Bug Fix
    Amended snowball_blue to allow it to work with the snowball.gd script like the red snowballs. Also Amended the snowball script to set a starting and largest scale for the snowballs.
    
    Player Characters were not colliding with the World Border so the Collision Layer for that Node was Changed.

--- snowball/snowball.gd
+++ snowball/snowball.gd
@@ -6,18 +6,17 @@ extends RigidBody3D
 @onready var collision_shape: CollisionShape3D = $"CollisionShape3D"
 
 func _ready():
-	model_pivot.scale = 2 * Vector3.ONE
-	collision_shape.scale = 2 * Vector3.ONE
-	
+	model_pivot.scale = 0.75 * Vector3.ONE
+	collision_shape.scale = 0.75 * Vector3.ONE
 
 func _physics_process(_delta):
 	var speed: float = linear_velocity.length()
 	if speed > 0:
 		var change: float = growth_constant * speed
 		increment_children_scale(change)
-		print(change)
 
 func increment_children_scale(change):
-	var new_scale_vec: Vector3 = change * Vector3.ONE
-	model_pivot.scale += new_scale_vec
-	collision_shape.scale += new_scale_vec
+	if model_pivot.scale < Vector3(4, 4, 4) and collision_shape.scale < Vector3(4, 4, 4):
+		var new_scale_vec: Vector3 = change * Vector3.ONE
+		model_pivot.scale += new_scale_vec
+		collision_shape.scale += new_scale_vec
